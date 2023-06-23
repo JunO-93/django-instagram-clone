d0bda39f9e7abf008e03148f1564da98f7fed414
JunO-93 <dlwnsdh17@naver.com>
2023-06-23 09:05:09 +0900

    #1 기본화면세팅

---
+++ junstagram/junstagram/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .settings import BASE_DIR
+
+class Sub(APIView):
+    def get(self, request):
+        print("get으로 호출")
+        print(BASE_DIR)
+        return render(request, 'junstagram/main.html')
+    
+    def post(self, request):
+        print("post으로 호출")
+        return render(request, 'junstagram/main.html')
+    
+    
